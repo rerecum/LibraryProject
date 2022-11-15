345aaf2fc97d7ea36fff5845074280e2070ba7c9
rerecum <kacperkacperkacper132@gmail.com>
2022-11-15 13:16:03 +0100

    GIT

--- LibraryProject/klienci.xaml.cs
+++ LibraryProject/klienci.xaml.cs
@@ -31,5 +31,10 @@ namespace LibraryProject
             f2.ShowDialog();
             this.Close();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
