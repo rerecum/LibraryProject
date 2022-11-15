27dca39d19b927d859e820393e94aca43d253477
rerecum <kacperkacperkacper132@gmail.com>
2022-11-15 14:38:19 +0100

    KOD DODANIA

--- LibraryProject/dodaj.xaml.cs
+++ LibraryProject/dodaj.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Markup;
 
 namespace LibraryProject
 {
@@ -30,6 +33,23 @@ namespace LibraryProject
             MainWindow f2 = new MainWindow();
             f2.ShowDialog();
             this.Close();
+        }
+
+        private void dodaj_Click(object sender, RoutedEventArgs e)
+        {
+            string myConnection = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\student\Source\Repos\LibraryProject\LibraryProject\Biblioteka.mdf; Integrated Security = True";
+            SqlConnection myConn = new SqlConnection(myConnection);
+            SqlDataAdapter myDataAdapter = new SqlDataAdapter();
+            SqlCommandBuilder cb = new SqlCommandBuilder(myDataAdapter);
+            myConn.Open();
+            DataSet ds = new DataSet();
+
+            SqlCommand cmd = new SqlCommand(myConnection);
+            cmd.Connection = myConn;
+            cmd.CommandText = "INSERT INTO [books](Tytul, Autor, Gatunek) values ('" + tytul.Text + "','" + autor.Text + "','" + gatunek.Text + "')";
+            cmd.ExecuteNonQuery();
+
+            myConn.Close();
         }
     }
 }
