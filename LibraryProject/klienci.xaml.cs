27c21f9cb2361d61cbb3b72b613f8bbf551fee77
rerecum <kacperkacper132@gmail.com>
2022-11-22 12:07:10 +0100

    NOWA TABELA

--- LibraryProject/klienci.xaml.cs
+++ LibraryProject/klienci.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,6 +36,22 @@ namespace LibraryProject
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+
+        }
+
+        private void DataGrid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            string myConnection = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\student\\source\\repos\\LibraryProject\\LibraryProject\\Biblioteka.mdf;Integrated Security=True";
+            SqlConnection myConn = new SqlConnection(myConnection);
+            SqlDataAdapter myDataAdapter = new SqlDataAdapter();
+            SqlCommandBuilder cb = new SqlCommandBuilder(myDataAdapter);
+            myConn.Open();
+            DataSet ds = new DataSet();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM [klienci]", myConn);
+            cmd.Connection = myConn;
+
+            var reader = cmd.ExecuteReader();
 
         }
     }
