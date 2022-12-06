18078f274dfe0b432cdeb6ed1f5d23fcf5af4efe
rerecum <kacperkacperkacper132@gmail.com>
2022-12-06 13:17:46 +0100

    uczen

--- LibraryProject/uczen.xaml.cs
+++ LibraryProject/uczen.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,7 +28,19 @@ namespace LibraryProject
 
         private void dodaj_Click(object sender, RoutedEventArgs e)
         {
+            string myConnection = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\student\Source\Repos\LibraryProject\LibraryProject\Biblioteka.mdf; Integrated Security = True";
+            SqlConnection myConn = new SqlConnection(myConnection);
+            SqlDataAdapter myDataAdapter = new SqlDataAdapter();
+            SqlCommandBuilder cb = new SqlCommandBuilder(myDataAdapter);
+            myConn.Open();
+            DataSet ds = new DataSet();
 
+            SqlCommand cmd = new SqlCommand(myConnection);
+            cmd.Connection = myConn;
+            cmd.CommandText = "INSERT INTO [klienci](Imie, Nazwisko, Klasa, PESEL, Id_ksiazki) values ('" + imie.Text + "','" + nazwisko.Text + "','" + klasa.Text + "','" + PESEL.Text + "', '" + id_ksiazki.Text + "')";
+            cmd.ExecuteNonQuery();
+
+            myConn.Close();
         }
     }
 }
